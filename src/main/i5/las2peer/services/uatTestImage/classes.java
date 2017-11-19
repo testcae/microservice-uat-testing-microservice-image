e8264ab43af7bb14aede2f6cef1163681e5d8664
ukimiawz <melisa.cecilia@yahoo.com>
2017-11-19 09:39:08 +0100

    Code regeneration/Model synchronization

--- src/main/i5/las2peer/services/uatTestImage/classes.java
+++ src/main/i5/las2peer/services/uatTestImage/classes.java
@@ -5,39 +5,6 @@ import org.json.simple.*;
 
 public class classes {
 
-    class imageMusic {
-
-    public imageMusic() {}
-    
-    private String imageName;
-
-    public void setimageName(String setValue) {
-        this.imageName = setValue;
-    }
-
-    public String getimageName() {
-        return this.imageName;
-    }
-    private String imageUrl;
-
-    public void setimageUrl(String setValue) {
-        this.imageUrl = setValue;
-    }
-
-    public String getimageUrl() {
-        return this.imageUrl;
-    }
-
-    public JSONObject toJSON() {
-
-        JSONObject jo = new JSONObject();
-        jo.put("imageName", this.imageName); 
-        jo.put("imageUrl", this.imageUrl); 
-
-        return jo;
-    }
-
-}
     class image {
 
     public image() {}
@@ -60,55 +27,22 @@ public class classes {
     public String getimageUrl() {
         return this.imageUrl;
     }
+    private int imageId;
+
+    public void setimageId(int setValue) {
+        this.imageId = setValue;
+    }
+
+    public int getimageId() {
+        return this.imageId;
+    }
 
     public JSONObject toJSON() {
 
         JSONObject jo = new JSONObject();
         jo.put("imageName", this.imageName); 
         jo.put("imageUrl", this.imageUrl); 
-
-        return jo;
-    }
-
-}
-    class music {
-
-    public music() {}
-    
-    private String musicName;
-
-    public void setmusicName(String setValue) {
-        this.musicName = setValue;
-    }
-
-    public String getmusicName() {
-        return this.musicName;
-    }
-    private int musicId;
-
-    public void setmusicId(int setValue) {
-        this.musicId = setValue;
-    }
-
-    public int getmusicId() {
-        return this.musicId;
-    }
-    private String musicUrl;
-
-    public void setmusicUrl(String setValue) {
-        this.musicUrl = setValue;
-    }
-
-    public String getmusicUrl() {
-        return this.musicUrl;
-    }
-
-    public JSONObject toJSON() {
-
-        JSONObject jo = new JSONObject();
-        jo.put("musicName", this.musicName); 
-        jo.put("musicId", this.musicId); 
-        jo.put("musicUrl", this.musicUrl); 
+        jo.put("imageId", this.imageId); 
 
         return jo;
     }
